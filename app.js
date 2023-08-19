b0258705847cd35dd5e8ba2b2092ad474f936a8e
Rishabh soni <80063042+RishabhSoni01@users.noreply.github.com>
2023-08-19 16:55:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const express=require('express');
+nstconst express=require('express');
 const app=express();
 const cookieParser = require('cookie-parser')
 const path = require("path");
+const cors=require("cors");
 
 if(process.env.NODE_ENV != "production"){
     require("dotenv").config({path:"backend/config/config.env"});
@@ -9,7 +10,7 @@ if(process.env.NODE_ENV != "production"){
 app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({limit: "50mb",extended:true}));
 app.use(cookieParser());
-
+app.use(cors());
 const post = require('./routes/post');
 const user = require('./routes/user');
 
@@ -20,7 +21,8 @@ app.use("/api/v1",user);
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+    res.setHeader("Access-control-Allow-Credentials","true");
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 });
 
 module.exports=app;
