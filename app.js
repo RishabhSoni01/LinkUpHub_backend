ec5bda72cacc9fd928b43a199f0dfcd9aec6a220
Rishabh soni <80063042+RishabhSoni01@users.noreply.github.com>
2023-08-20 20:56:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,12 +18,12 @@ app.use("/api/v1",post);
 app.use("/api/v1",user);
 
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+// app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res) => {
-    res.setHeader("Access-control-Allow-Credentials","true");
-    res.send("Api is running");
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//     res.setHeader("Access-control-Allow-Credentials","true");
+//     res.send("Api is running");
+//     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+// });
 
 module.exports=app;
